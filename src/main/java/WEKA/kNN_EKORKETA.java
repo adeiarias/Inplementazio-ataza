28620281d9bf4941c27e665b9b4b960687b6eae5
adeiarias <adeiariasupv@gmail.com>
2021-02-16 18:44:50 +0100

    KNN EKORKETA casi

--- src/main/java/WEKA/kNN_EKORKETA.java
+++ src/main/java/WEKA/kNN_EKORKETA.java
@@ -3,7 +3,6 @@ package WEKA;
 import weka.classifiers.Evaluation;
 import weka.classifiers.lazy.IBk;
 import weka.core.*;
-import weka.core.SelectedTag;
 import weka.core.converters.ConverterUtils;
 import weka.core.neighboursearch.LinearNNSearch;
 
@@ -14,39 +13,74 @@ public class kNN_EKORKETA {
     private static Instances data;
 
     public static void main(String[] args) throws Exception {
-        int kEstimatu = Integer.parseInt(args[1]);
-        datuak_Kargatu(args[0]);
-        K_Parametroaren_Ekorketa(kEstimatu);
+        int kEstimatu = Integer.parseInt("10");
+        datuak_Kargatu("");
+        //K_Parametroaren_Ekorketa(kEstimatu);
         parametroGuztienEkorketa(kEstimatu);
     }
 
     private static void parametroGuztienEkorketa(int kEstimatu) throws Exception {
-        LinearNNSearch linearNNSearch = new LinearNNSearch();
-        NormalizableDistance[] distantziak = new NormalizableDistance[3];
-        distantziak[0] = new EuclideanDistance();
-        distantziak[1] = new ManhattanDistance();
-        distantziak[2] = new MinkowskiDistance();
+        LinearNNSearch[] distantziak = distantziakLortu();
+        SelectedTag[] tags = tagsLortu();
 
         double maxMeasure = 0.0;
         int k = 1;
         int w = 0;
         IBk knn = new IBk();
+
         for(int i=1; i<=kEstimatu; i++){
             knn.setKNN(i);
-            for(int j=0; j<distantziak.length; j++){
-                linearNNSearch.setDistanceFunction(distantziak[j]);
-                knn.setNearestNeighbourSearchAlgorithm(linearNNSearch);
 
-                Evaluation evaluation = new Evaluation(data);
-                evaluation.crossValidateModel(knn,data,10,new Random(1));
-                if(evaluation.weightedFMeasure() > maxMeasure){
-                    maxMeasure = evaluation.weightedFMeasure();
-                    k = i;
-                    w = j;
+            for(int j=0; j<distantziak.length; j++){
+                knn.setNearestNeighbourSearchAlgorithm(distantziak[j]);
+
+                for(int l=0; l<tags.length; l++) {
+                    knn.setDistanceWeighting(tags[l]);
+
+                    Evaluation evaluation = new Evaluation(data);
+                    evaluation.crossValidateModel(knn, data, 10, new Random(1));
+                    if (evaluation.weightedFMeasure() > maxMeasure) {
+                        maxMeasure = evaluation.weightedFMeasure();
+                        k = i;
+                        w = j;
+                    }
                 }
             }
         }
         System.out.println("K hoberena -> " + k);
+    }
+
+    private static SelectedTag[] tagsLortu() {
+        SelectedTag[] tags = new SelectedTag[3];
+
+        SelectedTag none = new SelectedTag(IBk.WEIGHT_NONE,IBk.TAGS_WEIGHTING);
+        tags[0] = none;
+
+        SelectedTag inverse = new SelectedTag(IBk.WEIGHT_INVERSE,IBk.TAGS_WEIGHTING);
+        tags[1] = inverse;
+
+        SelectedTag similarity = new SelectedTag(IBk.WEIGHT_SIMILARITY,IBk.TAGS_WEIGHTING);
+        tags[2] = similarity;
+
+        return tags;
+    }
+
+    private static LinearNNSearch[] distantziakLortu() throws Exception {
+        LinearNNSearch[] dist = new LinearNNSearch[3];
+
+        LinearNNSearch euclide = new LinearNNSearch();
+        euclide.setDistanceFunction(new EuclideanDistance());
+        dist[0] = euclide;
+
+        LinearNNSearch manhattan = new LinearNNSearch();
+        manhattan.setDistanceFunction(new ManhattanDistance());
+        dist[1] = manhattan;
+
+        LinearNNSearch minkowski = new LinearNNSearch();
+        minkowski.setDistanceFunction(new MinkowskiDistance());
+        dist[2] = minkowski;
+
+        return dist;
     }
 
     private static void K_Parametroaren_Ekorketa(int kEstimatu) throws Exception {
